005b202c8de7975401e6de61cf561ba201fa5ad8
Nicolás López <nicolaslopezj@me.com>
2016-11-22 22:00:53 -0300

    initial

---
+++ spec/orionsoft-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import OrionsoftView from '../lib/orionsoft-view';
+
+describe('OrionsoftView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
